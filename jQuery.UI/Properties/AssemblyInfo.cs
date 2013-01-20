8fae993e32b7f6a8d5844f6b87c4f5b68c5ec7dc
Erik Källén <erik_kallen@hotmail.com>
2013-01-20 20:27:26 +0100

    Split to its own repo

---
+++ jQuery.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Saltarelle.jQuery.UI")]
+[assembly: AssemblyDescription("Saltarelle jQuery.UI Core API")]
+[assembly: ScriptAssembly("jQueryui")]
+
+[assembly: AssemblyCompany("Erik Källén")]
+[assembly: AssemblyProduct("Saltarelle jQuery.UI bindings")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
